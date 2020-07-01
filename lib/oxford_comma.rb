b1cadae238c7c640b68a909c6d6a459e50c7035d
Adi Skop <adiskop@gmail.com>
2020-07-01 13:51:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,9 @@ else
 array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
+
+
+
+#    a[-1]    # last item in the array
+#    a[-2:]   # last two items in the array
+#    a[:-2]   # everything except the last two items
